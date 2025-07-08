82476e4f70e1cde0d51b91766f05448582789136
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-07-08 21:05:28 +0200

    feat: initial hangar UI/UX design

--- src/Arkanis.Overlay.Components/Helpers/LinkHelper.cs
+++ src/Arkanis.Overlay.Components/Helpers/LinkHelper.cs
@@ -1,9 +1,16 @@
 namespace Arkanis.Overlay.Components.Helpers;
 
+using Domain.Models.Inventory;
+
 public static class LinkHelper
 {
     private static readonly string AssemblyName = typeof(LinkHelper).Assembly.GetName().Name ?? string.Empty;
 
     public static string GetPathToAsset(string relativeAssetPath)
         => $"_content/{AssemblyName}/assets/{relativeAssetPath}";
+
+    public static string Hangar(InventoryEntryId? entryId)
+        => entryId is not null
+            ? $"/hangar/{entryId.Identity.ToString()}"
+            : "/hangar";
 }
--- src/Arkanis.Overlay.Infrastructure/Data/Mappers/UexApiDtoMapper.cs
+++ src/Arkanis.Overlay.Infrastructure/Data/Mappers/UexApiDtoMapper.cs
@@ -384,6 +384,7 @@ internal partial class UexApiDtoMapper(IGameEntityHydrationService hydrationServ
     [MapProperty(nameof(VehicleDTO.Is_docking), nameof(GameVehicle.SupportsDocking))]
     [MapProperty(nameof(VehicleDTO.Is_loading_dock), nameof(GameVehicle.SupportsCargoDeck))]
     [MapProperty(nameof(VehicleDTO.Container_sizes), nameof(GameVehicle.MaxContainerSize), Use = nameof(MapMaxContainerSizeFromList))]
+    [MapProperty(nameof(VehicleDTO.Url_photo), nameof(GameVehicle.ImageUrl))]
     [MapPropertyFromSource("manufacturer", Use = nameof(GetCompanyForVehicle))]
     private partial GameSpaceShip MapInternalSpaceShip(VehicleDTO source);
 
@@ -395,6 +396,7 @@ internal partial class UexApiDtoMapper(IGameEntityHydrationService hydrationServ
     [MapProperty(nameof(VehicleDTO.Is_docking), nameof(GameVehicle.SupportsDocking))]
     [MapProperty(nameof(VehicleDTO.Is_loading_dock), nameof(GameVehicle.SupportsCargoDeck))]
     [MapProperty(nameof(VehicleDTO.Container_sizes), nameof(GameVehicle.MaxContainerSize), Use = nameof(MapMaxContainerSizeFromList))]
+    [MapProperty(nameof(VehicleDTO.Url_photo), nameof(GameVehicle.ImageUrl))]
     [MapPropertyFromSource("manufacturer", Use = nameof(GetCompanyForVehicle))]
     private partial GameGroundVehicle MapInternalGroundVehicle(VehicleDTO source);
 
