3367e24f3ef41ff05fb97aa47d9ab6e641da21c6
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-09-26 15:23:15 +0200

    fix: allow all internal data cache to be purged when reloading all data via cache management

--- src/Arkanis.Overlay.Infrastructure/Services/Abstractions/IChangeTokenManager.cs
+++ src/Arkanis.Overlay.Infrastructure/Services/Abstractions/IChangeTokenManager.cs
@@ -9,7 +9,15 @@ public interface IChangeTokenManager
     /// </summary>
     /// <typeparam name="T">Type to get the change token for.</typeparam>
     /// <returns>The <see cref="IChangeToken" /> associated with the given type.</returns>
-    IChangeToken GetChangeTokenFor<T>();
+    public IChangeToken GetChangeTokenFor<T>();
+
+    /// <summary>
+    ///     Gets the <see cref="IChangeToken" /> associated with the specified reference object of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the reference object.</typeparam>
+    /// <param name="reference">The reference object to get the change token for.</param>
+    /// <returns>The <see cref="IChangeToken" /> associated with the given reference object.</returns>
+    public IChangeToken GetChangeTokenFor<T>(T reference);
 
     /// <summary>
     ///     Triggers a change to the <see cref="IChangeToken" /> associated with the given type and returns the new
@@ -17,11 +25,26 @@ public interface IChangeTokenManager
     /// </summary>
     /// <typeparam name="T">Type to reset the change token for.</typeparam>
     /// <returns>The new <see cref="IChangeToken" /> associated with the given type.</returns>
-    Task<IChangeToken> ResetChangeTokenFor<T>();
+    public Task<IChangeToken> ResetChangeTokenForAsync<T>();
 
     /// <summary>
     ///     Triggers a change to the <see cref="IChangeToken" /> associated with the given type.
     /// </summary>
     /// <typeparam name="T">Type to reset the change token for.</typeparam>
-    Task TriggerChangeForAsync<T>();
+    public Task TriggerChangeForAsync<T>();
+
+    /// <summary>
+    ///     Triggers a change to the <see cref="IChangeToken" /> associated with the specified reference object of type <typeparamref name="T"/>
+    ///     and notifies any registered change callbacks.
+    /// </summary>
+    /// <typeparam name="T">The type of the reference object.</typeparam>
+    /// <param name="reference">The reference object to trigger the change for.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    public Task TriggerChangeForAsync<T>(T reference);
+
+    /// <summary>
+    ///     Triggers a change to all registered <see cref="IChangeToken" /> instances and notifies all registered change callbacks.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    public Task TriggerChangeForAllAsync();
 }
