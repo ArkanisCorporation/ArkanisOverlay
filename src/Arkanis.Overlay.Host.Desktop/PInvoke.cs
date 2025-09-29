ead69a06ddf914e57260fc69a6047f51757537b4
FatalMerlin <merlin.brandes@gmail.com>
2025-09-30 00:00:00 +0200

    feat: hopefully finalize notifications and HUD window and WindowTracker.cs fixes

--- src/Arkanis.Overlay.Host.Desktop/PInvoke.cs
+++ src/Arkanis.Overlay.Host.Desktop/PInvoke.cs
@@ -5,10 +5,46 @@ namespace Windows.Win32;
 
 using System.Threading;
 using Foundation;
+using global::System.Runtime.CompilerServices;
+using global::System.Runtime.InteropServices;
+using Microsoft.Extensions.Logging;
+using Serilog.Core;
 using UI.WindowsAndMessaging;
 
 internal partial class PInvoke
 {
+    public record struct GuardResult
+    {
+        public bool Success { get; init; }
+        public int ErrorCode { get; init; }
+        public string? ErrorMessage { get; init; }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static GuardResult Guard(BOOL result, ILogger? logger = null, string? pInvokeMethodName = null)
+    {
+        if (result != true) { return new GuardResult { Success = true }; }
+
+        pInvokeMethodName ??= "Unknown PInvoke Method";
+
+        var errorCode = Marshal.GetLastWin32Error();
+        var errorMessage = Marshal.GetLastPInvokeErrorMessage();
+
+        logger?.LogError(
+            "[{PInvokeMethodName}] PInvoke failed with error code {ErrorCode}: {ErrorMessage}",
+            pInvokeMethodName,
+            errorCode,
+            errorMessage
+        );
+
+        return new GuardResult
+        {
+            Success = false,
+            ErrorCode = errorCode,
+            ErrorMessage = errorMessage
+        };
+    }
+
     /// <summary>
     ///     Workaround for unsafe pointer access.
     ///     See: https://github.com/microsoft/CsWin32/issues/137#issuecomment-1879493081
@@ -47,7 +83,7 @@ internal partial class PInvoke
         var length = GetWindowTextLength(hWnd);
         Span<char> windowText = stackalloc char[length];
         GetWindowText(hWnd, windowText);
-        return SpanToString(windowText, length-1); // ignore trailing null terminator
+        return SpanToString(windowText, length - 1); // ignore trailing null terminator
     }
 
     public static bool IsTopLevelWindow(HWND hWnd)
