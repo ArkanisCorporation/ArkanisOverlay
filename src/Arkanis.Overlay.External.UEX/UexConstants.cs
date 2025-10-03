089975d0960b11bb54578eaadca72104db827bae
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2025-10-03 07:21:04 +0200

    fix: replace UEX assets URL to ensure images work, refactor link usage

--- src/Arkanis.Overlay.External.UEX/UexConstants.cs
+++ src/Arkanis.Overlay.External.UEX/UexConstants.cs
@@ -1,6 +1,10 @@
 namespace Arkanis.Overlay.External.UEX;
 
-internal static class UexConstants
+public static class UexConstants
 {
+    public const string WebBaseUrl = "https://uexcorp.space";
     public const string ApiBaseUrl = "https://uex-proxy.arkanis.space/2.0";
+
+    public const string AssetsBaseUrl = "https://assets.uexcorp.space/img";
+    public const string AssetsPreferredUrl = "https://assets.uexcorp.space/img";
 }
