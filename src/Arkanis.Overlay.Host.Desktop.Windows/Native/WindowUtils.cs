ec5da02f515c7700e87304060c914cb7fc7c4ce0
FatalMerlin <merlin.brandes@gmail.com>
2025-12-30 00:00:00 +0200

    docs: add XML documentation for window utility methods

--- src/Arkanis.Overlay.Host.Desktop.Windows/Native/WindowUtils.cs
+++ src/Arkanis.Overlay.Host.Desktop.Windows/Native/WindowUtils.cs
@@ -13,6 +13,10 @@ public class WindowUtils : IWindowUtils
 {
     private const WINDOW_EX_STYLE ClickThroughStyle = WINDOW_EX_STYLE.WS_EX_TRANSPARENT | WINDOW_EX_STYLE.WS_EX_LAYERED;
 
+    /// <summary>
+    /// Allows mouse clicks to pass through the specified window.
+    /// </summary>
+    /// <param name="window"></param>
     public void EnableClickThrough(Window window)
     {
         var hwnd = GetHwnd(window);
@@ -25,13 +29,18 @@ public class WindowUtils : IWindowUtils
         SetExtendedStyle(hwnd, ClickThroughStyle, true);
     }
 
+    /// <summary>
+    /// Updates the extended window style for the specified window with the given style options.
+    /// Can be used to en- or disable e.g. Layered window compositing or transparency.
+    /// </summary>
+    /// <param name="hwnd">The native Windows window handle. Can be obtained from an Avalonia <see cref="Window"/> using <see cref="GetHwnd"/>.</param>
+    /// <param name="extendedStyle">The extended window style flag enum value, one or more values combined using bit-wise OR.</param>
+    /// <param name="remove">If true, removes the style instead of adding it. Defaults to false.</param>
     private static void SetExtendedStyle(HWND hwnd, WINDOW_EX_STYLE extendedStyle, bool remove = false)
     {
         var exStyle = PInvoke.GetWindowLong(hwnd, WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE);
 
-        var newStyle = remove ?
-            exStyle & ~(int)extendedStyle :
-            exStyle | (int)extendedStyle;
+        var newStyle = remove ? exStyle & ~(int)extendedStyle : exStyle | (int)extendedStyle;
 
         var result = PInvoke.SetWindowLong(hwnd, WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE, newStyle);
 
@@ -41,6 +50,15 @@ public class WindowUtils : IWindowUtils
         }
     }
 
+    /// <summary>
+    /// Gets the native Windows HWND handle for the specified Avalonia window.
+    /// </summary>
+    /// <param name="window">The Avalonia window instance.</param>
+    /// <returns>The native Windows window handle.</returns>
+    /// <exception cref="NotSupportedException">
+    ///     Thrown when the Avalonia window does not have a Platform Handle.
+    ///     ( <see cref="Window.TryGetPlatformHandle"/> returned <c>null</c> )
+    /// </exception>
     private static HWND GetHwnd(Window window)
     {
         var iHandle = window.TryGetPlatformHandle();
